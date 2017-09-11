a0795138297971069b9a4fe9ababbf9bd1ecb776
Dmitry Bondarenko <d.bondrnko@gmail.com>
2017-09-11 13:35:42 +0300

    initial comit

---
+++ testing/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Hi there!
+      </div>
+    );
+  }
+}
+
+export default App;
