50b0a641c6f9903285bff260d17729589fa8a8ed
Dmitry Bondarenko <d.bondrnko@gmail.com>
2017-09-13 14:43:19 +0300

    initial commit of middleware section

---
+++ middleware/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app container">
+          Hey HO!
+      </div>
+    );
+  }
+}
+
+export default App;
