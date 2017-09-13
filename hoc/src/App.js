f5d8301a9801383519e8a43ab1a41d5e6e6bf863
Dmitry Bondarenko <d.bondrnko@gmail.com>
2017-09-13 14:18:32 +0300

    Ended making HOC with conditional rendering

--- hoc/src/App.js
+++ hoc/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "./components/header";
 import Resources from './components/resources';
 import Home from './components/home';
+import RequireAuth from './components/require_auth';
 
 import { Route, Switch } from 'react-router-dom';
 
@@ -11,7 +12,7 @@ class App extends Component {
             <div className="app">
                 <Header />
                 <Switch>
-                    <Route path='/resources' component={Resources} />
+                    <Route path='/resources' component={RequireAuth(Resources)} />
                     <Route path='/' component={Home} />
                 </Switch>
             </div>
--- hoc/src/components/require_auth.js
+++ hoc/src/components/require_auth.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 export default function (ComposedComponent) {
     class Auth extends React.Component {
         render() {
+            if (!this.props.isLoggedIn) {
+                return <Redirect push to='/'/>
+            }
             return <ComposedComponent {...this.props} />;
         }
     }
 
-    return Auth;
+    const mapStateToProps = ({isLoggedIn}) =>{
+        return {isLoggedIn}
+    };
+
+    return connect(mapStateToProps)(Auth);
 };
