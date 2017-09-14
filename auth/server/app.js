ba2a1babc117ed9214af29795c920af87d23a75b
Dmitry Bondarenko <d.bondrnko@gmail.com>
2017-09-14 17:16:51 +0300

    Making server-side authentication, model and encoing done

--- auth/server/app.js
+++ auth/server/app.js
@@ -2,10 +2,15 @@ const express = require("express"),
     app = express(),
     http = require("http"),
     morgan = require("morgan"),
+    router = require("./router"),
+    mongoose = require("mongoose"),
     bodyParser = require("body-parser");
+
+mongoose.connect("mongodb://localhost:auth/auth");
 
 app.use(morgan("combined"));
 app.use(bodyParser.json({ type: "*/*" }));
+router(app);
 
 const server = http.createServer(app),
     port = process.env.PORT || 8080;
---
+++ auth/server/controller/authenication.js
@@ -0,0 +1,35 @@
+const jwt = require("jwt-simple");
+const config = require("../config");
+const User = require("../models/user");
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime();
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signup = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (!email || !password) {
+        return res.status(422).send({ error: "You MUST provide Email AND Password" });
+    }
+
+    User.findOne({ email }, (err, existUser) => {
+        if (err) {
+            return next(err);
+        }
+        if (existUser) {
+            return res.status(422).send({ error: "Email is in use!" });
+        } else {
+            const newUser = new User({ email, password });
+            newUser.save((err) => {
+                if (err) {
+                    return next(err);
+                } else {
+                    res.json({ token: tokenForUser(newUser) });
+                }
+            });
+        }
+    });
+};
